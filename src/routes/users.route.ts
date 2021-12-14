4e4ddb60fbc41d0b008f68c01dff3c49211a82fa
Sidney Outeiro <sid@dcc.ufrj.br>
2021-12-14 18:08:40 -0300

    preparando para o review

--- src/routes/users.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as usersController from "../controllers/users.controller";
-
-const route = express.Router();
-
-route.post("", usersController.create);
-
-export default route;
--- src/schemas/student.schema.ts
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-const studentSchema = Joi.object({
-    name: Joi.string().min(3).max(30).required(),
-    class: Joi.string().min(2).max(10).required(),
-});
-
-export default studentSchema;
